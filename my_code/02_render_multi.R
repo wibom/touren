610dbced8f50dce620e48a9b815f476469e5044c
wibom <c.wibom@gmail.com>
2020-09-06 11:31:09 +0200

    add photos tour20

--- my_code/02_render_multi.R
+++ my_code/02_render_multi.R
@@ -14,6 +14,11 @@ d <- read_rds(
 players <- read_players_list() %>% filter(id != "Ingen") %>% pull(id)
 tours <- d$tour_id
 
+# re-processing settings:
+force_plr_reprocessing <- FALSE
+force_img_reprocessing <- FALSE
+force_res_reprocessing <- FALSE
+
 # Spelare ----
 #players <- players[1]
 for (player in players) {
@@ -21,26 +26,46 @@ for (player in players) {
     str_to_lower(player) %>%
     str_replace_all("[åä]", "a") %>%
     str_replace_all("[ö]", "o")
-  rmarkdown::render(
-    input = "my_code/player.Rmd",
-    output_file = player_formated,
-    output_dir = "content/players",
-    clean = FALSE,
-    run_pandoc = TRUE
+
+  # create player-page if
+  # 1) it does not already exist
+  #    - or -
+  # 2) it should be re-processed
+  plr_processed <- file.exists(
+    glue::glue("./content/players/{player_formated}.md")
   )
+  if(!plr_processed | force_plr_reprocessing) {
+    rmarkdown::render(
+      input = "my_code/player.Rmd",
+      output_file = player_formated,
+      output_dir = "content/players",
+      clean = FALSE,
+      run_pandoc = TRUE
+    )
+  }
 }
 
 
 # Bilder ----
-for(tour in tours) {
+tour_img <- c(tours, "tour20") # tour20; missing data, has photos
+for(tour in tour_img) {
+
   if(dir.exists(file.path(here::here(), 'static/img', tour, 'scaled'))){
-    rmarkdown::render(
-      input = "my_code/photo.Rmd",
-      output_file = paste0("photos_", tour),
-      output_dir = "content/pics",
-      clean = TRUE,
-      run_pandoc = TRUE
-    )
+
+    # create photo-page if
+    # 1) it does not already exist
+    #    - or -
+    # 2) it should be re-processed
+    img_processed <- file.exists(glue::glue("./content/pics/photos_{tour}.md"))
+    if(!img_processed | force_img_reprocessing) {
+      rmarkdown::render(
+        input = "my_code/photo.Rmd",
+        output_file = paste0("photos_", tour),
+        output_dir = "content/pics",
+        clean = TRUE,
+        run_pandoc = TRUE
+      )
+    }
   }
 }
 
@@ -50,14 +75,25 @@ results_var <- "results_net"
 abs_or_rel <- "rel"
 my_weights <- setNames(seq_along(rev(tours)), rev(tours))
 for(tour in tours) {
-  weight = my_weights[tour] %>% unname()
-  rmarkdown::render(
-    input = "my_code/tour.Rmd",
-    output_file = tour,
-    output_dir = "content/results",
-    clean = TRUE,
-    run_pandoc = TRUE
+
+  # create results-page if
+  # 1) it does not already exist
+  #    - or -
+  # 2) it should be re-processed
+  res_processed <- file.exists(
+    glue::glue("./content/results/{tour}.md")
   )
+  if(!res_processed | force_res_reprocessing) {
+
+    weight = my_weights[tour] %>% unname()
+    rmarkdown::render(
+      input = "my_code/tour.Rmd",
+      output_file = tour,
+      output_dir = "content/results",
+      clean = TRUE,
+      run_pandoc = TRUE
+    )
+  }
 }
 
 
